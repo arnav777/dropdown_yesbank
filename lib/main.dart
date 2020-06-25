57019bc9c3845f1bfb41231e1398743455aa4cdc
arnav777 <59831448+arnav777@users.noreply.github.com>
2020-06-25 11:36:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Menu Button Example',
-      home: ScreenFour(),
+      home: ScreenOne(),
     );
   }
 }
